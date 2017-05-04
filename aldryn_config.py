ef1e6d42f4b8873acf1667ee27780e2353a05e08
Daniele Procida <daniele@vurt.org>
2017-05-04 14:44:43 +0100

    Updated aldryn_config.py

--- aldryn_config.py
+++ aldryn_config.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 import os
 
@@ -6,51 +7,79 @@ from aldryn_client import forms
 
 class Form(forms.BaseForm):
     def to_settings(self, data, settings):
-        from oscar import defaults
-        from oscar import OSCAR_MAIN_TEMPLATE_DIR, get_core_apps
-        settings.update(vars(defaults))
-        settings['INSTALLED_APPS'].extend(
-            ['django.contrib.flatpages',
-             'widget_tweaks'
-             ] + get_core_apps()
-        )
-        settings['TEMPLATES'] = [
-            {
-                'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [
-                    os.path.join(settings['BASE_DIR'], 'templates'),
-                    OSCAR_MAIN_TEMPLATE_DIR
-                ],
-                'APP_DIRS': True,
-                'OPTIONS': {
-                    'context_processors': [
-                        'django.template.context_processors.debug',
-                        'django.template.context_processors.request',
-                        'django.contrib.auth.context_processors.auth',
-                        'django.contrib.messages.context_processors.messages',
+        
+        from oscar import OSCAR_MAIN_TEMPLATE_DIR
+        import oscar.defaults
+        
+        # Need to detect if these settings are for Django 1.8+
+        # Is there a better way? Can't import django to check version =(
+        is_django_18_or_later = ('TEMPLATES' in settings)
+        
+        
+        # import all of Oscar's default settings
+        settings.update(vars(oscar.defaults))
+        
+        # ------- TEMPLATES --------
+        if is_django_18_or_later:
+            
+            settings['TEMPLATES'][0]['DIRS'].append(oscar.OSCAR_MAIN_TEMPLATE_DIR)
+            # settings['TEMPLATES'][0]['APP_DIRS'] = True
+            settings['TEMPLATES'][0]['OPTIONS']['context_processors'].extend([
+                'oscar.apps.search.context_processors.search_form',
+                'oscar.apps.promotions.context_processors.promotions',
+                'oscar.apps.checkout.context_processors.checkout',
+                'oscar.apps.customer.notifications.context_processors.notifications',
+                'oscar.core.context_processors.metadata',                
+            ])
+        
+        else:
+            
+            settings['TEMPLATE_CONTEXT_PROCESSORS'].extend([
+                'oscar.apps.search.context_processors.search_form',
+                'oscar.apps.promotions.context_processors.promotions',
+                'oscar.apps.checkout.context_processors.checkout',
+                'oscar.apps.customer.notifications.context_processors.notifications',
+                'oscar.core.context_processors.metadata',                
+                
+            ])
+            settings['TEMPLATE_DIRS'].append(oscar.OSCAR_MAIN_TEMPLATE_DIR)
+            
 
-                        'oscar.apps.search.context_processors.search_form',
-                        'oscar.apps.promotions.context_processors.promotions',
-                        'oscar.apps.checkout.context_processors.checkout',
-                        'oscar.apps.customer.notifications.context_processors.notifications',
-                        'oscar.core.context_processors.metadata',
-                    ],
-                },
-            },
-        ]
+        # ------- INSTALLED_APPS --------
+        
+        # flatpages could be installed by other applications, so check before adding it
+        if not 'django.contrib.flatpages' in settings['INSTALLED_APPS']:
+             settings['INSTALLED_APPS'].append('django.contrib.flatpages')
+            
+        settings['INSTALLED_APPS'].extend([
+            'widget_tweaks'  # this is optional as far as Oscar is concerned
+            ] + oscar.get_core_apps()
+        )
+
+        # ------- MIDDLEWARE --------
+
         settings['MIDDLEWARE_CLASSES'].extend([
             'oscar.apps.basket.middleware.BasketMiddleware',
-            'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
         ])
-        settings['AUTHENTICATION_BACKENDS'] = (
-            'oscar.apps.customer.auth_backends.EmailBackend',
-            'django.contrib.auth.backends.ModelBackend',
-        )
+
+        # flatpages could be configured by other applications, so check first
+        if not 'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware' in settings['MIDDLEWARE_CLASSES']:
+             settings['MIDDLEWARE_CLASSES'].append('django.contrib.flatpages.middleware.FlatpageFallbackMiddleware')
+
+
+        # ------- Other settings --------
+
+        settings['AUTHENTICATION_BACKENDS'].append('oscar.apps.customer.auth_backends.EmailBackend')
+        
+        # need to check this
         settings['HAYSTACK_CONNECTIONS'] = {
             'default': {
                 'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
             },
         }
+        
+        # ------- URLs --------
         settings['ADDON_URLS'].append('aldryn_django_oscar.urls')
+
         return settings
         
